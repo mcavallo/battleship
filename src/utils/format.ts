a13274fd255b031c20c229219de84ce823c4b489
Mariano Cavallo <mariano.cavallo@gmail.com>
2024-05-24 13:14:24 +0200

    Add mising function descriptions

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,7 +1,13 @@
+/**
+ * Formats a decimal number keeping only 2 decimals and removes decimals from integers.
+ */
 export function formatDecimal(value: number) {
   return value.toFixed(2).replace(/\.?0+$/, '');
 }
 
+/**
+ * Pluralizes a string based on a count.
+ */
 export function pluralize(count: number, noun: string, suffix = 's') {
   return `${noun}${count !== 1 ? suffix : ''}`;
 }
