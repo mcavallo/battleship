639eeb27cff89644598ac0e5a4ad9309cc1ef2f9
Mariano Cavallo <mariano.cavallo@gmail.com>
2024-05-26 13:35:16 +0200

    Restructure css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 import 'react-tooltip/dist/react-tooltip.css';
-import './index.css';
+
+import './main.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
